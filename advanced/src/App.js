10c39088cc2fb4e90eb296e8a13152f034fb2ce8
Pratham <ppratham812@gmail.com>
2023-07-23 17:32:48 +0530

    files added

---
+++ advanced/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+// import ErrorExample from './tutorial/01-useState/errorExample';
+import UseStateBasics from './tutorial/01-useState/useStateBasics';
+function App() {
+  return (
+    <div className="App">
+      <UseStateBasics/> 
+    </div>
+  );
+}
+
+export default App;
